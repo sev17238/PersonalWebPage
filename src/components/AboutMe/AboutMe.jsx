8188be3cefc941aec5550aca524a868b78367058
Diego Sevilla <sevilla.diego8@gmail.com>
2021-01-05 18:40:31 -0600

    style: :art: center Experiences title and content

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -20,30 +20,30 @@ export default class AboutMe extends React.Component {
                     <div className='inner-div-AboutMe'>
                         <h3 className='title-AboutMe'>About me</h3>
                         <Row className='row'>
-                            <Col className='column-left'>
+                            <Col className='column-left-AboutMe'>
                                 <Slide left>
                                     <div className='column-div'>
-                                        <h4 className='h4-am'>Name: </h4>
-                                        <p className='p-am'>Diego Sevilla </p>
-                                        <h4 className='h4-am'>Age: </h4>
-                                        <p className='p-am'>23 </p>
-                                        <h4 className='h4-am'>Location: </h4>
-                                        <p className='p-am'>Guatemala City, Guatemala </p>
+                                        <h4 className='h4-AboutMe'>Name: </h4>
+                                        <p className='p-AboutMe'>Diego Sevilla </p>
+                                        <h4 className='h4-AboutMe'>Age: </h4>
+                                        <p className='p-AboutMe'>23 </p>
+                                        <h4 className='h4-AboutMe'>Location: </h4>
+                                        <p className='p-AboutMe'>Guatemala City, Guatemala </p>
                                     </div>
                                 </Slide>
                             </Col>
-                            <Col className='column-center'>
+                            <Col className='column-center-AboutMe'>
                                 <Zoom>
-                                    <div className='div-image'>
-                                        <img src={Me} className='image-me' alt="me"/>
+                                    <div className='div-image-AboutMe'>
+                                        <img src={Me} className='image-AboutMe' alt="me"/>
                                     </div>
                                 </Zoom>
                             </Col >
-                            <Col className='column-right'>
+                            <Col className='column-right-AboutMe'>
                                 <Slide right>
                                     <div className='column-div-2'>
-                                        <p className='p-am-des'>I&lsquo;m currently studying Computing Science Engeeniring
-                                        at <a className='anchor-am' href='https://www.uvg.edu.gt/'>Universidad del Valle de Guatemala</a>. I have this habit of being a perfectionist in 
+                                        <p className='p-AboutMe-description'>I&lsquo;m currently studying Computing Science Engeeniring
+                                        at <a className='anchor-AboutMe' href='https://www.uvg.edu.gt/'>Universidad del Valle de Guatemala</a>. I have this habit of being a perfectionist in 
                                         all the things I do, I like to spend time working on small details and giving my best to solve problems.
                                         </p>
                                     </div>
